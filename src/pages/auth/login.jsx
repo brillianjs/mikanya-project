c3dc10d3145f1bc76115a2719d21b0df1fb59790
brillianjs <jobrillian@gmail.com>
2024-01-17 23:43:46 +0700

    Add logo and update login page layout

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,7 +1,27 @@
+import { Input, Button, Heading } from "@chakra-ui/react";
+import Logo from "../../assets/logo.png";
+
 export default function LoginPage() {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="w-full flex h-screen justify-center items-center">
+      <div className="flex flex-col">
+        {/* logo */}
+        <span className="flex flex-row justify-center items-center">
+          <img className="w-16" src={Logo} alt="logo-image" />
+          <Heading>mikanya</Heading>
+        </span>
+        {/* form */}
+        <div className="max-w-sm container flex flex-col border p-5 rounded-lg">
+          <form action="" className="space-y-4">
+            <Input placeholder="Username" type="username" />
+            <Input placeholder="Password" type="password" />
+            <Button colorScheme="purple" color={"white"} width={"full"}>
+              {" "}
+              Login
+            </Button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
