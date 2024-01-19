4db24c2b7586506069a51596cf1ab4492bc5f7f7
brillianjs <jobrillian@gmail.com>
2024-01-20 00:08:54 +0700

    Add chart.js and react-chartjs-2 dependencies

---
+++ src/pages/dashboard/index.jsx
@@ -0,0 +1,9 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export default function DashboardPage() {
+  return (
+    <div className="w-screen">
+      <Heading>Hello world this is dashboard page</Heading>
+    </div>
+  );
+}
---
+++ src/pages/speedtest/index.jsx
@@ -0,0 +1,3 @@
+export default function SpeedtestPage() {
+  return <div>hello world this is speedtest page</div>;
+}
