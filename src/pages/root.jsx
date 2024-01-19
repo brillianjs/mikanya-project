3748dd8f365b38cac7d5c22f77be493ab7dd6a7e
brillianjs <jobrillian@gmail.com>
2024-01-20 00:08:43 +0700

    Add sidebar and main content to RootPage

--- src/pages/root.jsx
+++ src/pages/root.jsx
@@ -1,11 +1,25 @@
 import { Outlet } from "react-router";
 import NavbarComponent from "../components/navbar";
+import { Box, Grid, GridItem, Flex } from "@chakra-ui/react";
+import { Link as ReactRouterLink } from "react-router-dom";
+import { Link as ChakraLink } from "@chakra-ui/react";
 
 export default function RootPage() {
   return (
     <>
       <NavbarComponent />
-      <Outlet />
+      <Flex>
+        {/* Sidebar */}
+        <Box w="250px" h="100vh">
+          Sidebar Content
+        </Box>
+
+        {/* Main Content */}
+        <Box p="4">
+          {/* Isi konten disini */}
+          Main Content
+        </Box>
+      </Flex>
     </>
   );
 }
