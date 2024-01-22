034116e19ead182be4acfc62401f361c11e9007b
brillianjs <jobrillian@gmail.com>
2024-01-22 21:44:43 +0700

    Refactor Chakra Form component styles

--- src/utils/chakra/theme.js
+++ src/utils/chakra/theme.js
@@ -11,37 +11,6 @@ const activeLabelStyles = {
 const theme = extendTheme({
   initialColorMode: "light",
   useSystemColorMode: false,
-  components: {
-    Form: {
-      variants: {
-        floating: {
-          container: {
-            _focusWithin: {
-              label: {
-                ...activeLabelStyles,
-              },
-            },
-            "input:not(:placeholder-shown) + label, .chakra-select__wrapper + label, textarea:not(:placeholder-shown) ~ label":
-              {
-                ...activeLabelStyles,
-              },
-            label: {
-              top: 0,
-              left: 0,
-              zIndex: 2,
-              position: "absolute",
-              backgroundColor: "white",
-              pointerEvents: "none",
-              mx: 3,
-              px: 1,
-              my: 2,
-              transformOrigin: "left top",
-            },
-          },
-        },
-      },
-    },
-  },
 });
 
 export default theme;
