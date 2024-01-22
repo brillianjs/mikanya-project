70ecd2f054fee1bf595fbbd3fb9222d904bf403a
brillianjs <jobrillian@gmail.com>
2024-01-22 22:06:51 +0700

    Add user icon and quick print QR option to profile edit page

--- src/pages/dashboard/profile-edit.jsx
+++ src/pages/dashboard/profile-edit.jsx
@@ -6,23 +6,39 @@ import {
   Select,
   Input,
   Button,
+  Divider,
 } from "@chakra-ui/react";
+import { FaRegUser } from "react-icons/fa";
 
 const ProfileEdit = () => {
   return (
     <div className="w-full">
-      <Card p={5}>
+      <Card p={5} gap={6}>
+        <span className="flex flex-row items-center  gap-3">
+          <FaRegUser />
+          <Heading fontSize={20} className="flex flex-row">
+            Admin
+          </Heading>
+        </span>
+        <Divider />
         <form action="" className="flex flex-col gap-4">
           {/* <Heading fontSize={20}>Profile Edit</Heading> */}
-          <FormControl variant="floating" id="first-name" isRequired>
+          <FormControl id="first-name" isRequired>
             <FormLabel>First name</FormLabel>
             <Input placeholder=" " />
           </FormControl>
-          <FormControl variant="floating" id="password" isRequired>
+          <FormControl id="password" isRequired>
             <FormLabel>Password</FormLabel>
             <Input placeholder=" " type="password" />
           </FormControl>
-          <Button>Save</Button>
+          <FormControl>
+            <FormLabel>Cetak Cepat QR</FormLabel>
+            <Select placeholder="Cetak Cepat QR">
+              <option value="option1">Enable</option>
+              <option value="option2">Disable</option>
+            </Select>
+          </FormControl>
+          <Button colorScheme="purple">Save</Button>
         </form>
       </Card>
     </div>
