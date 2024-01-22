8bc57f026bb2e0457dd7045a76cbdc7e8219d222
brillianjs <jobrillian@gmail.com>
2024-01-22 21:44:33 +0700

    Add form labels and form control to login page

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,4 +1,10 @@
-import { Input, Button, Heading } from "@chakra-ui/react";
+import {
+  Input,
+  Button,
+  Heading,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
 import Logo from "../../assets/logo.png";
 import { useNavigate } from "react-router-dom";
 
@@ -6,7 +12,7 @@ export default function LoginPage() {
   const navigate = useNavigate();
   return (
     <div className="w-full flex h-screen justify-center items-center">
-      <div className="flex flex-col">
+      <div className="flex flex-col w-full justify-center items-center">
         {/* logo */}
         <span className="flex flex-row justify-center items-center">
           <img className="w-16" src={Logo} alt="logo-image" />
@@ -14,9 +20,20 @@ export default function LoginPage() {
         </span>
         {/* form */}
         <div className="max-w-sm container flex flex-col border p-5 rounded-lg">
-          <form action="" className="space-y-4">
-            <Input placeholder="Username" type="username" />
-            <Input placeholder="Password" type="password" />
+          <form action="" className="space-y-4 w-full">
+            {/* Username Input */}
+            <FormControl>
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <Input id="username" placeholder="Username" type="username" />
+            </FormControl>
+
+            {/* Password Input */}
+            <FormControl>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <Input id="password" placeholder="Password" type="password" />
+            </FormControl>
+
+            {/* Login Button */}
             <Button
               colorScheme="purple"
               color={"white"}
